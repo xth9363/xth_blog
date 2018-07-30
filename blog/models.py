897bed85f33d8fbcf57cc6dc480e54e15ca51003
pc <xth4065@gmail.com>
2018-07-30 10:06:32 +0800

    display 10

--- blog/models.py
+++ blog/models.py
@@ -197,12 +197,10 @@ class Visitor(models.Model):
         verbose_name_plural = '访问者'
 
     ip = models.GenericIPAddressField(verbose_name='访问者Ip')
-    url = models.TextField(verbose_name='访问地址',null=True,blank=True)
+    url = models.TextField(verbose_name='访问地址', null=True, blank=True)
     user = models.ForeignKey(UserProfile, null=True, blank=True, verbose_name='用户', on_delete=models.CASCADE)
     visit_date = models.DateTimeField(auto_now_add=True, verbose_name='访问时间')
     location = models.CharField(max_length=64, verbose_name='访问者所在地', null=True, blank=True)
 
     def __str__(self):
         return "{}|{}|{}".format(self.id, self.ip, self.location)
-
-
--- blog/templatetags/my_tags.py
+++ blog/templatetags/my_tags.py
@@ -34,11 +34,11 @@ def set_url_var(*args, **kwargs):
 def side_bar(*args, **kwargs):
     # 返回侧边栏数据
     side_data = {
-        'new5': Article.objects.order_by('-add_date').all()[:5],
-        'read5': Article.objects.order_by('-read_times').all()[:5],
+        'new5': Article.objects.order_by('-add_date').all()[:10],
+        'read5': Article.objects.order_by('-read_times').all()[:10],
         'tag12': ArticleTag.objects.annotate(num_articles=Count('articles')).filter(num_articles__gt=0).order_by(
             '-num_articles'),
-        'article_group': ArticleGroup.objects.order_by('-add_date').all()[:5]
+        'article_group': ArticleGroup.objects.order_by('-add_date').all()[:10]
     }
     return side_data
 
